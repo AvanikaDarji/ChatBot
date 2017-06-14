f4c5b5a4a8db5eb40816c1c7dc2f8bb1c1f7be73
Avanika <avanikadarji@gmail.com>
2017-06-13 17:36:34 -0700

    Registration and Login

--- app.js
+++ app.js
@@ -1,33 +1,82 @@
 var express = require("express");
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var expressValidator = require('express-validator');
+
+//intialize app
 var app = express();
 var port = 3000;
-var mongoose = require("mongoose");
 
+var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Users')
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
-var Schema = new mongoose.Schema({
-  email: String,
-  password: String
-});
 
-//var dbURI = 'mongodb://localhost/Users';
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-//mongoose.connect(dbURI);
+// set up view engine with ejs for templating
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); 
 
- 
-var User = mongoose.model('User',Schema);
 
- 
-
+// body parser middleware
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+//set static folder
+app.use(express.static(path.join(__dirname, '/public')));
 
-//home page
+//Middleware for  express session
+app.use(session({
+  secret:'secret',
+  saveUninitialized:true,
+  resave:true
+
+}));
+//passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connect flash middleware
+
+app.use(flash());
+app.use('/', routes);
+app.use('/users', users);
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+/*//home page
     app.get('/', function(req, res) {
      
        res.render('index.ejs');
@@ -36,7 +85,7 @@ app.set('view engine', 'ejs'); // set up ejs for templating
    
     // show the login form
     app.get('/login', function(req, res) {
-        res.render('login.ejs')
+        res.render('login.ejs');
     });
 
 
@@ -47,16 +96,16 @@ app.set('view engine', 'ejs'); // set up ejs for templating
 
 // process the signup form
 
-app.post('/signup', (req, res) => {
+app.post('/signup', function(req, res) {
   var userData = new User(req.body);
   userData.save()
     .then(item => {
-      res.send("Data saved to database");
+      res.send("You have registered successfully");
     })
     .catch(err => {
       res.status(400).send("unable to save to database");
     });
-});
+});*/
 
 app.listen(port, () => {
   console.log("Server listening on port " + port);
@@ -65,19 +114,7 @@ module.exports = app;
 
 
 
-//var configDB = require('./config/db.js');
 
-//var dbURI = 'mongodb://localhost/Users';
-//mongoose.connect(configDB.url);
-
-//mongoose.connection.on('connected', function() {
-  //  console.log('Mongoose is connected to ' + configDB.url);
-//});
-
-// var User = require('./Application/models/User.js');
-
-// set up our express application
-// get information from html forms
 
 
 
