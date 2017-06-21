1f9981b02345bf5a5fc29b10a81a3e0cdb5fd2dd
Avanika <avanikadarji@gmail.com>
2017-06-20 18:18:55 -0700

    added db

--- models/user.js
+++ models/user.js
@@ -17,7 +17,19 @@ var UserSchema = mongoose.Schema({
 	},
 	name: {
 		type: String
-	}
+	},
+	/* facebook         : {
+        id           : String,
+        token        : String,
+        email        : String,
+        name         : String
+    },
+    google           : {
+        id           : String,
+        token        : String,
+        email        : String,
+        name         : String
+	}*/
 });
 var User =  mongoose.model('User', UserSchema);
 module.exports = User;
